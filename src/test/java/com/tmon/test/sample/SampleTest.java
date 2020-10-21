2284e9da9ab4db1c4cef3fd29e322a7904b164d5
nrkim <828!>
2020-10-21 11:18:55 +0900

    first commit

---
+++ src/test/java/com/tmon/test/sample/SampleTest.java
@@ -0,0 +1,13 @@
+package com.tmon.test.sample;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+
+    @Test
+    public void test(){
+        System.out.println("sample test success");
+    }
+
+}
